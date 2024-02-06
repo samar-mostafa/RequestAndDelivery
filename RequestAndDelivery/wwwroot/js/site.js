539db2c54b9ee11317daba79242f56af0f0698c5
samar.it <samar.it@SAMAR>
2024-02-06 10:53:37 +0200

    some edits

--- RequestAndDelivery/wwwroot/js/site.js
+++ RequestAndDelivery/wwwroot/js/site.js
@@ -36,7 +36,7 @@ function getEmployeeData(inp) {
 
 $(document).ready(function () {
     //datetimepicker
-    if ($('').length > 0) {
+    if ($('.js-datePicker').length > 0) {
         $('.js-datePicker').daterangepicker({
             singleDatePicker: true,
             maxDate: new Date(),
@@ -77,47 +77,47 @@ $(document).ready(function () {
     });
 })
 
-window.addEventListener('DOMContentLoaded', event => {
+//window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+//    // Navbar shrink function
+//    var navbarShrink = function () {
+//        const navbarCollapsible = document.body.querySelector('#mainNav');
+//        if (!navbarCollapsible) {
+//            return;
+//        }
+//        if (window.scrollY === 0) {
+//            navbarCollapsible.classList.remove('navbar-shrink')
+//        } else {
+//            navbarCollapsible.classList.add('navbar-shrink')
+//        }
 
-    };
+//    };
 
-    // Shrink the navbar 
-    navbarShrink();
+//    // Shrink the navbar 
+//    navbarShrink();
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+//    // Shrink the navbar when page is scrolled
+//    document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
+//    // Activate Bootstrap scrollspy on the main nav element
+//    const mainNav = document.body.querySelector('#mainNav');
+//    if (mainNav) {
+//        new bootstrap.ScrollSpy(document.body, {
+//            target: '#mainNav',
+//            rootMargin: '0px 0px -40%',
+//        });
+//    };
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-});
+//    // Collapse responsive navbar when toggler is visible
+//    const navbarToggler = document.body.querySelector('.navbar-toggler');
+//    const responsiveNavItems = [].slice.call(
+//        document.querySelectorAll('#navbarResponsive .nav-link')
+//    );
+//    responsiveNavItems.map(function (responsiveNavItem) {
+//        responsiveNavItem.addEventListener('click', () => {
+//            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                navbarToggler.click();
+//            }
+//        });
+//    });
+//});
